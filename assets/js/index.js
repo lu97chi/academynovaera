3446fc7cb08e0ae919efb50d867fc767a7f07d11
Luis Roberto Hernández Robles <lu97is@gmail.com>
2018-06-02 00:44:54 -0600

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -172,7 +172,7 @@ $('#vid4s').on('click', function(){
 })
 
 $('#messengerChat').on('click', ()=>{
-  window.location = 'https://goo.gl/NuhcQ9' 
+  window.location = 'http://bit.ly/Messenger-NovaEra' 
 })
 $('#register').on('click', ()=>{
   window.location = 'https://www.academynovaera.com/registro.html'
